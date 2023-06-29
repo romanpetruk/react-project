ea0f10a5658e4e675f1910a209c0d92338b49caf
Roman Petruk <romanpetruk.r7@gmail.com>
2023-06-29 20:13:37 +0300

    added custom hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import PostFilter from "./components/PostFilter";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyModal from "./components/UI/MyModal/MyModal";
-
+import { usePosts } from "./components/hooks/usePosts";
 
 
 import './styles/App.css'
@@ -19,21 +19,11 @@ function App() {
     );
     
     const [filter, setFilter] = useState({sort:'', query:''})
-    
+    const sortedAndSearchPosts = usePosts(posts,filter.sort, filter.query);
     const [modal, setModal] = useState(false)
     
 
-    const sortedPost = useMemo(()=>{
-      console.log('render');
-      if(filter.sort){
-        return [...posts].sort((a,b)=>a[filter.sort].localeCompare(b[filter.sort]))
-      }
-      return posts
-    },[filter.sort,posts]);
-
-    const sortedAndSearchPosts = useMemo(()=>{
-      return sortedPost.filter(post=>post.title.toLowerCase().includes(filter.query))
-    },[filter.query, sortedPost])
+    
 
     const createPost = (newPost)=>{
     setPosts([...posts, newPost]);
